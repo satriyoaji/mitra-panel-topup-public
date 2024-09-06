6ceafdcfcd72af1b28095b7cd524497253ede8ff
Riza Bachtiar <riza.bachtiar.rb@gmail.com>
2024-09-06 14:36:14 +0700

    feat: add note transaction

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -42,7 +42,7 @@ const CommandInput = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>
 >(({ className, ...props }, ref) => (
   <div
-    className="flex items-center border rounded mx-4 px-3"
+    className="flex items-center border rounded m-2 px-3"
     cmdk-input-wrapper=""
   >
     <MagnifyingGlassIcon className="mr-2 h-4 w-4 shrink-0 opacity-50" />
